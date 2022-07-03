3f2fa47f4ce8cb96169c8294309778fea0ef9c5e
Seif Yehia <SeifYehiaDev@outlook.com>
2022-07-03 20:52:17 +0200

    Trick in unsigned data type out of range.

---
+++ Unit_Two_C_Programming/lesson_3 _C_Basics/Sammary/OutOfRange.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <limits.h>
+
+void main() {
+    const unsigned int minUnsignedInt = 0, maxUnsignedInt = UINT_MAX;
+    unsigned int negativeNumber = -63, resultOfEquation;
+    printf("\nThe range of unsigned int from %u to %u\n", minUnsignedInt, maxUnsignedInt);
+    resultOfEquation = maxUnsignedInt + (negativeNumber + 1);
+    printf("\"negativeNumber = %u\"   &   \"resultOfEquation = %u\"\n", negativeNumber, resultOfEquation);
+    if (negativeNumber == resultOfEquation)
+        printf("negativeNumber is equal resultOfEquation\n");
+    else
+        printf("negativeNumber is not equal resultOfEquation\n");
+}
