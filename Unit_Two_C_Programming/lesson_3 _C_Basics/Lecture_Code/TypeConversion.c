8d9f5aea3f37b7a9dbc75c48a08e3b9d060e1950
Seif Yehia <SeifYehiaDev@outlook.com>
2022-07-04 21:33:16 +0200

    This code in unit2_3 @1:29 Type Conversion Assignments

---
+++ Unit_Two_C_Programming/lesson_3 _C_Basics/Lecture_Code/TypeConversion.c
@@ -0,0 +1,29 @@
+/*
+ * TypeConversion.c
+ *
+ * Created on : 7/4/2022
+ * Author : Seif Yehia Esmail Abdel Aal
+ * */
+
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+
+void main() {
+    float a = 1.0, b = 2.0, c = 3.0;
+    int s;
+    s = a * b * c / 100 + 32 / 4 - 3 * 1.1;
+    printf("s = %d", s); // s = 4
+
+    /* Solution for this equation .
+     * s = a * b * c / 100 + 32 / 4 - 3 * 1.1;
+     * s = 6.0 / 100 + 32 / 4 - 3 * 1.1 ;
+     * s = 0.06 + 32 / 4 - 3 * 1.1 ;
+     * s = 0.06 + 8 - 3 * 1.1 ;
+     * s = 0.06 + 8 - 3.3 ;
+     * s = 8.06 - 3.3 ;
+     * s = 4.76 ;
+     * s = 4 ; ======> Convert float to int.
+     * */
+}
