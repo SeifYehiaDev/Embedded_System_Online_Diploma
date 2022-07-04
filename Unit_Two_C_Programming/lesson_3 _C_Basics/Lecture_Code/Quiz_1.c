69d4d3ababdb46fa6fa644207b780e0c191ae844
Seif Yehia <SeifYehiaDev@outlook.com>
2022-07-04 18:47:14 +0200

    This code in unit_2_3 @1:20:30 Quiz What is the output ?

---
+++ Unit_Two_C_Programming/lesson_3 _C_Basics/Lecture_Code/Quiz_1.c
@@ -0,0 +1,21 @@
+/*
+ * Quiz_1.c
+ *
+ * Created on : 7/4/2022
+ * Author : Seif Yehia Esmail Abdel Aal
+ * */
+
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+
+void main() {
+    // What is the output ?
+    int x = 5;
+    float y = 2.0;
+    if (x / y == 2)
+        printf("int / float ===> int.\n");
+    else if (x / y == 2.5) //======> This condition is satisfied.
+        printf("int / float ===> float.\n");
+}
