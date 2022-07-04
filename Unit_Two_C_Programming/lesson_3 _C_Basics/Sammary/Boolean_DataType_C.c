a41ffb356fbda33f17e5fe6d7c3d2e002694a135
Seif Yehia <SeifYehiaDev@outlook.com>
2022-07-04 18:35:09 +0200

    This code explain boolean data type in C programming.

---
+++ Unit_Two_C_Programming/lesson_3 _C_Basics/Sammary/Boolean_DataType_C.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+
+void main() {
+    bool isYes = true, isNo = false;
+    printf("isYes = %d\nisNo = %d\n", isYes, isNo);
+}
