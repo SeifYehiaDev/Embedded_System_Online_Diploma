e6650bb2f9d2de135765b208eac4fb37f6b5516d
Seif Yehia <SeifYehiaDev@outlook.com>
2022-07-05 03:57:29 +0200

    This code Explain how to print address in memory for variable..

---
+++ Unit_Two_C_Programming/lesson_3 _C_Basics/Lecture_Code/MemoryAddressLocation.c
@@ -0,0 +1,13 @@
+/*
+ * MemoryAddressLocation.c
+ *
+ * Created on : 5/7/2022
+ * Author : Seif Yehia Esmail Abdel Aal
+ * */
+
+#include <stdio.h>
+
+void main() {
+    int intNumber = 45 ;
+    printf("Memory Address location for intNumber is : %d",&intNumber);
+}
