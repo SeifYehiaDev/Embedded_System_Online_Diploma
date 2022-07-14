37528fad004c6a826f051c43274dbb8fce67bc2b
Seif Yehia <SeifYehiaDev@outlook.com>
2022-07-15 01:32:10 +0200

    any commit now

---
+++ Unit_Two_C_Programming/lesson_3 _C_Basics/Lecture_Code/Lab_4.1.c
@@ -0,0 +1,24 @@
+/*
+ * Lab_4.1.c
+ *
+ * Created on : 15/7/2022
+ * Author : Seif Yehia Esmail Abdel Aal
+ *
+ * */
+
+#include <stdio.h>
+
+void main() {
+    /*                                          Question in unite two in lecture 3 page 105 .
+     *Calculate the summation of values between 1 and 99. using while loop.
+ * */
+    //                                                 _Solution_
+    int sumOfValue = 0, index = 0;
+    while (index <= 99) {
+        sumOfValue += index;
+        index++;
+    }
+    printf("Summation of values between (1 and 99) is : %d\n", sumOfValue);
+    //                                                 _Output_
+    // Summation of values between (1 and 99) is : 4950
+}
