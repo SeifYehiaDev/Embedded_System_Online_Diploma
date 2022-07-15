5fd95192bf5ebc36687848fee33f068f7557fb0a
Seif Yehia <SeifYehiaDev@outlook.com>
2022-07-15 02:37:05 +0200

    This code explain do while

---
+++ Unit_Two_C_Programming/lesson_3 _C_Basics/Lecture_Code/DoWhile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main() {
+    char userChoice = 'y';
+    float x, y;
+    do {
+        printf("Enter x value :\n");
+        fflush(stdin);
+        fflush(stdout);
+        scanf("%f", &x);
+        y = 5 * x * x + 3 * x + 2;
+        printf("y(%f) = %f\n", x, y);
+        printf("Do you want to evaluate again (y/n) ?\n");
+        fflush(stdin);
+        fflush(stdout);
+        scanf("%c", &userChoice);
+    } while (userChoice == 'y');
+    printf("Thank for you , End The program\n");
+}
