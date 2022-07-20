d54426933decf6c3415e66bfb98bebd29a7eb179
Seif Yehia <SeifYehiaDev@outlook.com>
2022-07-20 05:30:49 +0200

    this code explain how initialize value of array

---
+++ Unit_Two_C_Programming/Lesson_4_C_Array&String/Summary/2_InitialValueOfArray.c
@@ -0,0 +1,29 @@
+/*
+ * 2_InitialValueOfArray.c
+ *
+ * Created on : 20/7/2022
+ * Author : Seif Yehia Esmail Abdel Aal
+ *
+ * */
+
+#include <stdio.h>
+
+/*                                      One dimension Array
+ * initialization of array
+ * this code explain how initialize value of array
+ * */
+void main() {
+    int index;
+    int intOfArray[] = {1, 2, 3, 4};
+    printf("intOfArray[] = ");
+    for (index = 0; index <= 4 - 1; index++)
+        printf("%d ", intOfArray[index] );
+    double doubleOfArray[4] = {1.0, 2.0, 3.0, 4.0};
+    printf("\ndoubleOfArray[4] = ");
+    for (index = 0; index <= 4 - 1; index++)
+        printf("%f ", doubleOfArray[index] );
+}
+/*                                                 _Output_
+ * intOfArray[] = 1 2 3 4
+ * doubleOfArray[4] = 1.000000 2.000000 3.000000 4.000000
+ * */
