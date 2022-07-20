a5aeff00924a006c08871d0cb1b73bbc3d339a70
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2022-07-20 16:32:17 +0200

    this code explain how to print string without percentage s

--- Unit_Two_C_Programming/Lesson_4_C_Array&String/Summary/5_PrintStringWithoutPercentageS.c
+++ Unit_Two_C_Programming/Lesson_4_C_Array&String/Summary/5_PrintStringWithoutPercentageS.c
@@ -9,14 +9,14 @@
 #include <stdio.h>
 
 /*                                      String
- * this code explain how to print string without percentage s.
+ * this code explain how to print string without percentage s
  * */
 void main() {
     char array[] = {'h', 'e', 'l', 'l', 'o', 0};
     printf("%s \n", &array);
     // write code work as printf(%s)
     int index;
-    for (index = 0; index <= ((sizeof(char) * 8) - 1) && array[index] != 0; index++)
+    for (index = 0; index <= sizeof(array) && array[index] != 0; index++)
         printf("%c", array[index]);
 }
 /*                                                 _Output_
