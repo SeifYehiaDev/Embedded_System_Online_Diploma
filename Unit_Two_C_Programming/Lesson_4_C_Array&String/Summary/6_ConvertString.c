39ebcfa935d90b040d6eb7e4a9f774d29611c374
Seif Yehia <SeifYehiaDev@outlook.com>
2022-07-21 12:46:16 +0200

    Convert uppercase string to lowercase string and lowercase to uppercase ,without using strlwr and strupr

---
+++ Unit_Two_C_Programming/Lesson_4_C_Array&String/Summary/6_ConvertString.c
@@ -0,0 +1,40 @@
+/*
+ * 6_ConvertStringWithoutStrlwrAndStrupr.c
+ *
+ * Created on : 21/7/2022
+ * Author : Seif Yehia Esmail Abdel Aal
+ *
+ * */
+
+#include <stdio.h>
+#include <string.h>
+
+/*                                      String
+ * Convert uppercase string to lowercase string and lowercase to uppercase ,without using strlwr() & strupr()
+ * */
+void main() {
+    int index;
+    char name[] = "SeIf YeHia EsMaiL ABDEl AaL";
+    printf("\n_____________________________ The original name is _____________________________\n");
+    printf("%s\n", name);
+    printf("_____________________________ After Convert to upper _____________________________\n");
+    for (index = 0; name[index] != 0; index++)
+        if (name[index] >= 'a' && name[index] <= 'z') // Small letter
+            name[index] -= ('a' - 'A');
+    printf("%s\n", name);
+    printf("_____________________________ After Convert to lower _____________________________\n");
+    for (index = 0; name[index] != 0; index++)
+        if (name[index] >= 'A' && name[index] <= 'Z') // Capital letter
+            name[index] += ('a' - 'A');
+    printf("%s\n", name);
+
+
+}
+/*                                                 _Output_
+ * _____________________________ The original name is _____________________________
+ * SeIf YeHia EsMaiL ABDEl AaL
+ * _____________________________ After Convert to upper _____________________________
+ * SEIF YEHIA ESMAIL ABDEL AAL
+ * _____________________________ After Convert to lower _____________________________
+ * seif yehia esmail abdel aal
+ * */
